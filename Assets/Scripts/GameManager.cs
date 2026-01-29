6eeeedbdad84c945f696928ef63b682361e68bd3
duc <nguyenminhduc@dtu.edu.vn>
2026-01-30 05:59:43 +0700

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject roadPrefab; // Prefab của đoạn đường
+    public GameObject obstaclePrefab; // Prefab của chướng ngại vật
+    public int numOfRoads = 10; // Số đoạn đường ban đầu
+    Vector3 firstPosition = new Vector3(0, 0, 0); // Vị trí bắt đầu tạo đường
+    Vector3 nextPosition; // Vị trí tiếp theo để tạo đoạn đường
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nextPosition = firstPosition;
+        for (int i = 0; i < numOfRoads; i++)
+        {
+            CreateBasicRoad(i >= 2); // tạo 5 đoạn đường đầu tiên không có chướng ngại vật
+        }
+    }
+    public void CreateBasicRoad(bool hasObstacle)
+    {
+        // tạo ra con đường ban đầu với khoảng 10 đoạn đường
+        GameObject basicRoad = Instantiate(roadPrefab, nextPosition, Quaternion.identity);
+        // lấy vị trí đặt road tiếp theo ở trong nextPosition của roadPrefab kế trước
+        nextPosition = basicRoad.transform.Find("NextRoadPosition").transform.position;
+        if(!hasObstacle) return;
+        // tạo một obstacle lên road vừa tạo tại 1 trong 3 điểm là left, right, center
+        int randomPos = Random.Range(0, 3); // 0-left, 1-center, 2-right
+        Vector3 obstaclePosition = basicRoad.transform.Find(randomPos == 0 ? "Left" : randomPos == 1 ? "Right" : "Center").transform.position;
+        if (obstaclePrefab != null)
+        {
+            GameObject obstacle = Instantiate(obstaclePrefab, obstaclePosition, Quaternion.identity);
+            obstacle.transform.parent = basicRoad.transform; // đặt obstacle làm con của road để dễ quản lý
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    float turnInput = 0f;
+    GameManager gameManager;
+    public float forwardSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameManager = FindAnyObjectByType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turnInput = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(turnInput * 5, rb.linearVelocity.y, forwardSpeed);
+        if (rb.linearVelocity.y < 0) return;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // kiểm tra xem đối tượng player trigger vào có phải là checkpoint không
+        if (other.gameObject.CompareTag("TriggerArea"))
+        {
+            Debug.Log("Checkpoint reached!");
+            // Thực hiện các hành động khi chạm vào checkpoint ở đây
+            // tạo ra đoạn đường mới
+            gameManager.CreateBasicRoad(true);
+            // xóa đoạn đường đã đi qua sau một khoảng thời gian
+            Destroy(other.transform.parent.gameObject, 1f);
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Hit an obstacle! Game Over.");
+            // Thực hiện các hành động khi va chạm với chướng ngại vật ở đây
+            // Ví dụ: Dừng trò chơi, hiển
+            //Time.timeScale = 0f; // Dừng trò chơi
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Tải lại cảnh hiện tại để bắt đầu lại trò chơi
+        }
+    }
+}
